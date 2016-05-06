4e586e525684c0ea723e023f1609a4eac5e7a095
C.T. Lin <chentsulin@gmail.com>
2016-05-06 17:46:56 +0800

    Put airbnb after require yoctol-base

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
-    'eslint-config-airbnb',
     'eslint-config-yoctol-base',
     'eslint-config-yoctol-base/rules/strict',
+    'eslint-config-airbnb',
     './rules/react',
     './rules/react-a11y',
   ].map(require.resolve),
