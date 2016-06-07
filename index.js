27989407c7872c8bbc5ab4a87cf40d773dcf7c66
C. T. Lin <chentsulin@gmail.com>
2016-06-07 16:03:44 +0800

    Fix rules overwrited by airbnb

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: [
     'eslint-config-yoctol-base',
     'eslint-config-yoctol-base/rules/strict',
-    'eslint-config-airbnb',
+    'eslint-config-airbnb/rules/react',
+    'eslint-config-airbnb/rules/react-a11y',
     './rules/react',
     './rules/react-a11y',
   ].map(require.resolve),
