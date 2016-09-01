33017ed48b185b99a8c5c8c4fb2f63c356aebd34
C.T. Lin <chentsulin@gmail.com>
2016-09-01 14:35:59 +0800

    update

--- rules/react.js
+++ rules/react.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     // Prevent usage of deprecated methods
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
-    'react/no-deprecated': [1, { react: '15.0.0' }],
+    'react/no-deprecated': ['warning'],
   },
 
   settings: {
