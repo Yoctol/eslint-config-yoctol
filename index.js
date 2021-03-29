fce029f11fa1517c6d51aed14d64ddabb4f5999b
tw0517tw <tw0517tw@gmail.com>
2021-03-29 17:17:11 +0800

    chore: bump deps, update peerDeps

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
     'eslint-config-airbnb/rules/react-a11y',
     './rules/react',
     './rules/react-a11y',
-    'eslint-config-prettier/react',
   ].map(require.resolve),
   rules: {},
 };
